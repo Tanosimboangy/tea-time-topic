3aaeaa67a0e0f37f32cb3da6d2dd7d900bd610c3
Tanosimboangy <jacquit.aja@onja.org>
2020-10-02 16:10:53 +0300

    adding the data into local storage

---
+++ readme.md
@@ -0,0 +1,24 @@
+# TEA TIME TOPIC PROJECT
+
+## I In a few sentences, explain the structure of my project:
+- First of all, I fetched the data from the given link `"https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c70331ca4a16bb99/raw/6da767327041de13693181c2cb09459b0a3657a1/topics.json"` and map through each one of the list. After that. I displayed the data and translated all of the numeric number into readable date.
+- Secondly, I mirrored and stored all the data into the local storage, this is quite useful so that you do not need to fetch everytime.
+
+
+## II If I had more time, the area of my project I would like to improve:
+- I would improve the filtering and the sorting areas 
+- I want to improve the way for fetching the data outside of a scoped function
+
+## III Anything new I learn while working on this project:
+- I learned how to
+
+## VI The most challenging part for me were:
+- The most challenging parts for me were the filtering, sorting, and the importing the files by using module
+
+## V New explanation I would like about a specific topic:
+- I really still want more explanation about fetching, sorting, and filtering
+- I want to go in deep about when we should use dispatched event in local storage and when we should not
+
+## VI Any other comments
+- If possible, we want to have more explanation about the videos, how we can apply the new conceps we see in those videos
+- I know it is a bit challenging but the time was quite short for me, honnestly I am really slow.
--- script.js
+++ script.js
@@ -3,134 +3,118 @@ import { form, input, submiBtn, nextTopicsLists, pastTopicsLists } from './lib/e
 
 // Declaring the API link as a variable
 const endpoints = "https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c70331ca4a16bb99/raw/6da767327041de13693181c2cb09459b0a3657a1/topics.json";
-let items;
-
+let items = [];
 // Fetching the data form the API
 async function fetchdata() {
     const response = await fetch(endpoints);
     const data = await response.json();
-    items = data;
-    // A bit problem of the update
+    items.push(...data);
     console.log(items);
-
-    // Mapping through the items
-    function showLists(items) {
-        return items.map(person => {
-            return `
-            <article data-id="${person.id}" value="${person.id}" class="article">
-                <ul>
-                    <li>
-                        <ul class="title">
-                            <li><p>${person.title}</p></li>
-                            <li><button type="button" class="acrchive"> Archive </button></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <ul>
-                            <li><button value="${person.id}" type="button" id="increase">Increase</button></li>
-                            <li id="upvotes">${person.upvotes}</li>
-                        </ul>
-                        <ul>
-                            <li><button type="button" value="${person.id}" id="decrease">Decrease</button></li>
-                            <li id="downvotes">${person.downvotes}</li>
-                        </ul>
-                    </li>
-                </ul>
-            </article>
-            `
-        }).join("");
-    }
-    // DISPLAY THE LISTS
-    const showPeople = () => {
-        const html = showLists(items);
-        nextTopicsLists.innerHTML = html;
-        nextTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
-    }
-    showPeople();
-
-    //***************** LOCAL STORAGE *********************// 
-
-    // Storing the data into the local storage
-    const initLocalStorage = () => {
-        const stringFromLS = localStorage.getItem('items');
-        const lsItems = JSON.parse(stringFromLS);
-        if (lsItems) {
-            items = lsItems;
-            showPeople(items);
-        }
-        nextTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
-    };
-    const updateLocalStorage = () => {
-        localStorage.setItem('items', JSON.stringify(items));
-    };
-    // ADDING EVENTLISTENER IN THE LOCALSTORAGE
-    nextTopicsLists.addEventListener("listUpdated", updateLocalStorage);
-    initLocalStorage();
-
-    // *************** ADDING NEW LIST ******************//
-
-    // ADDING NEW LIST
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        console.log(e.target.input.value);
-        const el = e.target;
-        const inputValue = el.input.value;
-
-        // CREATING A NEW ITEM
-        const newList = {
-            id: Date.now(),
-            title: inputValue,
-            downvotes: 0,
-            discussedOn: "",
-            upvotes: 0,
-        }
-        // PUSH THE NEW ITEM INNTO THE ITEMS LISTS
-        items.push(newList);
-        nextTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
-        showPeople(items);
-    });
-
-    function decrease(e) {
-        const El = e.target;
-        const decreaseBtn = El.matches("#decrease");
-        if (decreaseBtn) {
-            const upvotesEl = El.matches("#upvotes");
-            // console.log(upvotesEl);
-            // addUpvotes();
-        }
-        console.log(decreaseBtn);
-
-        // const increaseBtn = El.matches(".increase");
-        // if (increaseBtn) {
-        //     const downvotesEl = El.matches(".downvotes");
-        //     // console.log(downvotesEl);
-        // }
-        // console.log(increaseBtn);
-    }
-
-    // const addUpvotes = (e) => {
-
-    // }
-
-    window.addEventListener('click', decrease);
+    showLists(items);
 }
 
+// Mapping through the items
+function showLists(items) {
+    const notdDiscussed = items.filter(person => !person.discussedOn);
+    const sortTheList = notdDiscussed.sort(function(a, b) {
+        const plusa = a.upvotes;
+        const minusa = a.downvotes;
+        const differncea = plusa - minusa;
+        const plusb = b.upvotes;
+        const minusb = b.downvotes;
+        const differenceb = plusb - minusb;
+        return differenceb - differncea;
+      });
+    const html = sortTheList.map(person => {
+    return `<article data-id="${person.id}" value="${person.id}" class="article">
+            <ul>
+                <li>
+                    <ul class="title">
+                        <li><p>${person.title}</p></li>
+                        <li><button type="button" class="acrchive"> Archive </button></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li><button type="button" class="increase">Increase</button></li>
+                        <li><span>${person.upvotes}<span></li>
+                    </ul>
+                    <ul>
+                        <li><button type="button" class="decrease">Decrease</button></li>
+                        <li>${person.downvotes}</li>
+                    </ul>
+                </li>
+            </ul>
+        </article>
+        `
+    }).join("");
+    nextTopicsLists.innerHTML = html;
+
+    const discussedTopics = items.filter(person => person.discussedOn);
+    const newHMTL = discussedTopics.map(person => {
+    return `
+        <article data-id="${person.id}" value="${person.id}" class="article">
+            <ul>
+                <li>
+                    <ul class="title">
+                        <li><p>${person.title}</p></li>
+                        <li><li><button type="button" class="delete">Delete</button></li></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li><button type="button" class="decrease">Decrease</button></li>
+                    </ul>
+                </li>
+            </ul>
+        </article>`}).join("");
+    pastTopicsLists.innerHTML = newHMTL;
+}
+
+
+//***************** LOCAL STORAGE *********************// 
+
+// Storing the data into the local storage
+const initLocalStorage = () => {
+    const stringFromLS = localStorage.getItem('items');
+    const lsItems = JSON.parse(stringFromLS);
+    if (lsItems) {
+        items = lsItems;
+        showLists(items);
+    }
+    nextTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
+    pastTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
+};
+const updateLocalStorage = () => {
+    localStorage.setItem('items', JSON.stringify(items));
+};
+// ADDING EVENTLISTENER IN THE LOCALSTORAGE
+nextTopicsLists.addEventListener("listUpdated", updateLocalStorage);
+pastTopicsLists.addEventListener("listUpdated", updateLocalStorage);
+
+
+//*************** ADDING NEW LIST ******************//
+
+// ADDING NEW LIST
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target.input.value);
+    const el = e.target;
+    const inputValue = el.input.value;
+
+    // CREATING A NEW ITEM
+    const newList = {
+        id: Date.now(),
+        title: inputValue,
+        downvotes: 0,
+        discussedOn: "",
+        upvotes: 0,
+    }
+    // PUSH THE NEW ITEM INNTO THE ITEMS LISTS
+    items.push(newList);
+    nextTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
+    showLists(items);
+});
+
 fetchdata();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
