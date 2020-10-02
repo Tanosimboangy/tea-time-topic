a101b9da5452bc7f38aaeb1f201e08594e0384f5
Tanosimboangy <jacquit.aja@onja.org>
2020-10-02 16:34:04 +0300

    adding the data into local storage

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ import { form, input, submiBtn, nextTopicsLists, pastTopicsLists } from './lib/e
 // Declaring the API link as a variable
 const endpoints = "https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c70331ca4a16bb99/raw/6da767327041de13693181c2cb09459b0a3657a1/topics.json";
 let items = [];
+
 // Fetching the data form the API
 async function fetchdata() {
     const response = await fetch(endpoints);
     const data = await response.json();
     items.push(...data);
-    console.log(items);
     showLists(items);
 }
 
@@ -49,7 +49,7 @@ function showLists(items) {
         `
     }).join("");
     nextTopicsLists.innerHTML = html;
-
+    // Filter for the discuseed ones
     const discussedTopics = items.filter(person => person.discussedOn);
     const newHMTL = discussedTopics.map(person => {
     return `
@@ -81,6 +81,7 @@ const initLocalStorage = () => {
     if (lsItems) {
         items = lsItems;
         showLists(items);
+        console.log(items);
     }
     nextTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
     pastTopicsLists.dispatchEvent(new CustomEvent('listUpdated'));
@@ -88,6 +89,7 @@ const initLocalStorage = () => {
 const updateLocalStorage = () => {
     localStorage.setItem('items', JSON.stringify(items));
 };
+initLocalStorage();
 // ADDING EVENTLISTENER IN THE LOCALSTORAGE
 nextTopicsLists.addEventListener("listUpdated", updateLocalStorage);
 pastTopicsLists.addEventListener("listUpdated", updateLocalStorage);
@@ -98,7 +100,6 @@ pastTopicsLists.addEventListener("listUpdated", updateLocalStorage);
 // ADDING NEW LIST
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(e.target.input.value);
     const el = e.target;
     const inputValue = el.input.value;
 
@@ -116,5 +117,28 @@ form.addEventListener("submit", (e) => {
     showLists(items);
 });
 
+function handleClick(e) {
+    if (e.target.matches(".increase")) {
+        const plusButton = e.target.dataset.id;
+        items.map(person => {
+          if (person.id === plusButton) {
+            person.upvotes++;
+          }
+        })
+        showLists(items);
+      }
+    
+      if (e.target.matches(".decrease")) {
+        const minusButton = e.target.dataset.id;
+        listOfTopics.map(person => {
+          if (person.id === minusButton) {
+            person.downvotes--;
+          }
+        })
+        showLists(items);
+      }
+}
+
+window.addEventListener("submit", handleClick);
 fetchdata();
 
